cc22a4068ce64d9feaaa53d045e62524cb3f00ff
GUWCH <945893335@qq.com>
2020-09-11 09:29:55 +0800

    defaultProps

--- src/Kreact/index.js
+++ src/Kreact/index.js
@@ -1,15 +1,13 @@
 import { TEXT } from "./const";
 
 function createElement(type, config, ...children) {
-  console.log("====================================");
-  console.log(config.__self);
-  console.log("====================================");
   if (config) {
     delete config.__self;
     delete config.__source;
   }
-
+  // 几内亚比绍
   let props = {
+    ...type.defaultProps,
     ...config,
     children: children.map((child) =>
       typeof child === "object" ? child : createText(child)
